3a142e3709442992290dc14b12605e0bb9885e2f
Leonardo Souza Martins <LSMartins@santoandre.sp.gov.br>
2022-11-03 10:22:46 -0300

    Commit inicial com propriedades SuperHero

---
+++ SuperHeroAPI/SuperHero.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperHeroAPI
+{
+    public class SuperHero
+    {
+        //declarando as propriedades
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+
+    }
+}
